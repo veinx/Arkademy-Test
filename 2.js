beb93c5345cb081422408b8681fe60b7500ac94a
veinx <60257772+veinx@users.noreply.github.com>
2020-01-25 14:12:05 +0700

    2.js
    method/function untuk memvalidasi username dan password

---
+++ 2.js
@@ -0,0 +1,10 @@
+const is_username_valid = username =>
+    /^[a-z][a-z0-9]{4,12}/.test(username);
+
+const is_password_valid = password =>
+    /{9,}/.test(password) && password.includes("!@#$%^&*_=.,+?");
+
+console.log(is_username_valid("adminivan"));
+console.log(is_username_valid("adm1n1v4n"));
+console.log(is_password_valid("4dM1n!v4n"));
+console.log(is_password_valid("1V4n@dmin"));
